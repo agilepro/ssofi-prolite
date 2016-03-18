a4b726d9ed59e5c24a3a1c79424762e4619d4f20
Keith Swenson <github@kswenson.oib.com>
2016-03-19 07:25:00 +0900

    clean up build config

--- build/build_configuration.example.bat
+++ build/build_configuration.example.bat
@@ -1,43 +1,28 @@
-:#####################################################################################################
+:#############################################################################
 :#
 :# Java home
 :#
-:#####################################################################################################
-set JAVA_HOME=c:\Program Files\Java\jdk1.5.0_11\
+:#############################################################################
+set "JAVA_HOME=c:\Program Files\Java\jdk1.8.0_20\"
 
-:#####################################################################################################
-:#
-:# Path to jar file containing javax.servlet.* classes
-:#
-:# e.g.:
-:# For Tomcat 4.1
-:# set SERVLET_API_CP="D:\Program Files\Apache Software Foundation\Tomcat 4.1\common\lib\servlet.jar"
-:#
-:# For Tomcat 5.5
-:# set SERVLET_API_CP="C:\Apache_Tomcat-5.5.23\common\lib\servlet-api.jar;C:\Apache_Tomcat-5.5.23\common\lib\jsp-api.jar"
-:#
-:#####################################################################################################
-set SERVLET_API_CP="c:\Program Files\ApacheTomcat6.0\lib\servlet-api.jar;c:\Program Files\ApacheTomcat6.0\lib\jsp-api.jar"
-
-:#####################################################################################################
+:#############################################################################
 :#
 :# Path to source directory.
 :#
-:#####################################################################################################
-set SOURCE_DIR=d:\sandbox\ps\openid
+:#############################################################################
+set "SOURCE_DIR=c:\GitHub\ssofi-prolite\trunk\"
 
-:#####################################################################################################
+:#############################################################################
 :#
-:# Path to ant installation.
+:# Path to ant installation. 
+:# If Ant is installed and in your executable path, then set to nothing
 :#
-:#####################################################################################################
-set ANT_DIR=d:\sandbox\ps\nugen\gwt\apache-ant-1.8.1\bin\
+:#############################################################################
+set "ANT_DIR=c:\Program Files\apache-ant-1.9.4\bin\"
 
-:#####################################################################################################
+:#############################################################################
 :#
-:# Path to build directory. nugen.war will be created here.
-:# TARGET_DIR_DRIVE should have the drive letter of TARGET_DIR - a kludge till we have a smarter script
+:# Path to build directory. the war will be created here.
 :#
-:#####################################################################################################
-set TARGET_DIR=d:\sandbuild\openid
-set TARGET_DIR_DRIVE=D:
+:#############################################################################
+set "TARGET_DIR=c:\build\ssofiProvider\"
